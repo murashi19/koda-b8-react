8af5357f378c26d7c8aecb67046a2209eb9875a1
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-07 12:01:36 +0700

    fix: fixing axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,83 +5,15 @@ const api = axios.create({
   // baseURL: `http://localhost:8081`,
 });
 
-let isRefreshing = false;
-let failedQueue = [];
-
-// const processQueue = (error, token = null) => {
-//   failedQueue.forEach((promise) => {
-//     if (error) {
-//       promise.reject(error);
-//     } else {
-//       promise.resolve(token);
-//     }
-//   });
-
-//   failedQueue = [];
-// };
-
 api.interceptors.request.use(
   (config) => {
     const accessToken = localStorage.getItem("access_token");
-
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error),
-);
-
-api.interceptors.response.use(
-  (response) => response,
-
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response?.status !== 401) {
-      return Promise.reject(error);
-    }
-    if (originalRequest._retry) {
-      return Promise.reject(error);
-    }
-    originalRequest._retry = true;
-    if (isRefreshing) {
-      return new Promise((resolve, reject) => {
-        failedQueue.push({ resolve, reject });
-      })
-        .then((token) => {
-          originalRequest.headers.Authorization = `Bearer ${token}`;
-          return api(originalRequest);
-        })
-        .catch((err) => Promise.reject(err));
-    }
-    isRefreshing = true;
-    // try {
-    //   const refreshToken = localStorage.getItem("refresh_token");
-
-    //   const response = await axios.post(`http://localhost:8081/auth/refresh`, {
-    //     refresh_token: refreshToken,
-    //   });
-    //   const newAccessToken = response.data.result.access_token;
-
-    //   localStorage.setItem("access_token", newAccessToken);
-    //   processQueue(null, newAccessToken);
-    //   originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-    //   return api(originalRequest);
-    // } catch (err) {
-    //   processQueue(err);
-
-    //   localStorage.removeItem("access_token");
-    //   localStorage.removeItem("refresh_token");
-
-    //   window.location.href = "/auth/login";
-
-    //   return Promise.reject(err);
-    // } finally {
-    //   isRefreshing = false;
-    // }
-  },
 );
 
 export default api;
