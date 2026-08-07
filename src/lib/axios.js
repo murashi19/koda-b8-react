224b35376758814fafd411df739b9aa6df6fe473
Muhamad Rafli Shidiq <muhamadraflishidiq@gmail.com>
2026-08-07 11:33:22 +0700

    fix: fixing baseURL API

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_BACKEND_URL,
-  baseURL: `http://localhost:8081`,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  // baseURL: `http://localhost:8081`,
 });
 
 let isRefreshing = false;
